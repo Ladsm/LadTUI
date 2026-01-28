1e8bdfade5ede119e07765f235c28a86d21fae3b
Ladsm <BarryKotler0@gmail.com>
2026-01-28 23:25:02 +0200

    Made example better.

--- Examples/Calculator Example.cpp
+++ Examples/Calculator Example.cpp
@@ -20,6 +20,10 @@ public:
 			result = num1 * num2;
 			break;
 		case '/':
+			if (num2 == 0) {
+				std::cout << "Math Error : Cant divide by zero\n";
+				return 0;
+			}
 			result = num1 / num2;
 			break;
 		default:
@@ -38,7 +42,7 @@ int main() {
 	ladtui::Menu Cmenu;
 	ladtui::NumericUpDown Cupdown;
 	calculator c;
-	std::string items[5] = { "Add", "Subtrack", "Multiplication", "Devide", "Exit"};
+	std::string items[5] = { "Add", "Subtract", "Multiplication", "Divide", "Exit"};
     std::vector<std::function<void()>> OperationMenu = {
         [&operation]() { operation = '+'; },
         [&operation]() { operation = '-'; },
@@ -47,17 +51,16 @@ int main() {
 		[]() { exit(1); }
     };
 	tils.DelaySeconds(2);
-start:
-	tils.ClearScreen();
-	tils.BackgroundDraw(3);
-	std::cout << "Calculator application\n";
-	num1 = Cupdown.useNumupdown();
-	Cmenu.MenuMenu(items, OperationMenu, 5);
-	num2 = Cupdown.useNumupdown();
-	c.calculate(num1, operation, num2);
-	std::cout << c.result << '\n';
-	tils.DelaySeconds(1);
-	goto start;
+	while (true) {
+		tils.ClearScreen();
+		tils.BackgroundDraw(3);
+		std::cout << "Calculator application\n";
+		num1 = Cupdown.useNumupdown();
+		Cmenu.MenuMenu(items, OperationMenu, 5);
+		num2 = Cupdown.useNumupdown();
+		c.calculate(num1, operation, num2);
+		std::cout << num1 << operation << num2 << " is " << c.result << '\n';
+		tils.DelaySeconds(1);
+	}
 	return 0;
-
 }
